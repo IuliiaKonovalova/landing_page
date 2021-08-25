78c75d87266333b4b211d43782a94f3cbaaf237b
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-08-25 14:19:35 +0800

    Add active class on navbar, Style navbar on click

--- script.js
+++ script.js
@@ -4,8 +4,11 @@
 
 const hamburger = document.getElementById('hamburger');
 
+const navbar = document.getElementById('navbar');
+
 const openMenu = function () {
      hamburger.classList.add('active');
+     navbar.classList.add('active');
 };
 
 hamburger.addEventListener('click', openMenu);
