3c067e6534812942664f27f19844ba4050b65b82
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-08-25 13:43:48 +0800

    Restructure hamburger

---
+++ script.js
@@ -0,0 +1,11 @@
+// // 'use strict';
+
+// //Variables
+
+// const hamburger = document.getElementById('nav-toggle-label');
+
+// const openMenu = function () {
+//      hamburger.classList.add('active');
+// };
+
+// hamburger.addEventListener('click', openMenu());
