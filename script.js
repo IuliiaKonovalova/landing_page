2a23e7c016772fcd6bd11a50c06a3ea0ef601fc8
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-08-26 20:12:52 +0800

    Add event listener keydown for Escape to close menu

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ const hamburger = document.getElementById('hamburger');
 
 const navbar = document.getElementById('navbar');
 
+const navLink = document.getElementsByClassName('nav-link');
+
 //Function openMenu
 
 const openMenu = function () {
      hamburger.classList.add('active');
+
+     //  hamburger.classList.add('close');
      navbar.classList.add('active');
 };
 
@@ -20,4 +24,16 @@ const closeMenu = function () {
      navbar.classList.remove('active');
 };
 
+// 1. Event Open Mobile Menu:
+
 hamburger.addEventListener('click', openMenu);
+
+// 2. Event Close Mobile Menu:
+
+// navLink.forEach((link) => link.addEventListener('click', closeMenu));
+
+document.addEventListener('keydown', function (e) {
+     if (e.key === 'Escape' && hamburger.classList.contains('active')) {
+          closeMenu();
+     }
+});
