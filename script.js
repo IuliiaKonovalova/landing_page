0c31d904079237586481e106b3a1d2cc0a010a9b
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-08-26 19:22:54 +0800

    Add comments for each function in the js file

--- script.js
+++ script.js
@@ -6,10 +6,14 @@ const hamburger = document.getElementById('hamburger');
 
 const navbar = document.getElementById('navbar');
 
+//Function openMenu
+
 const openMenu = function () {
      hamburger.classList.add('active');
      navbar.classList.add('active');
 };
+
+//Function closeMenu
 
 const closeMenu = function () {
      hamburger.classList.remove('active');
