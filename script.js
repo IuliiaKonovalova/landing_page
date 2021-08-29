3225cd3ce7090ae886b88bb84510670799ddf858
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-08-29 19:18:35 +0800

    Add variables for gallery elements in js

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 
 //Variables;
 
+// 1. for navbar
+
 const hamburger = document.getElementById('hamburger');
 
 const navbar = document.getElementById('navbar');
@@ -10,7 +12,20 @@ const navLink = document.getElementsByClassName('nav-link');
 
 let menuOpen = false;
 
-// Mobile Menu:
+// 2. for gallery
+
+const carouselTrack = document.querySelector('.carousel__track--top');
+const slides = document.querySelectorAll('.carousel__slide');
+
+const prevButton = document.querySelector('#carousel__button--prev');
+const nextButton = document.querySelector('#carousel__button--next');
+
+const carouselNav = document.querySelector('.carousel__nav');
+const dots = document.querySelectorAll('.carousel__indicator');
+
+//
+///////////////    Mobile Menu:   /////////////////
+//
 
 // 1. Function Open/Close Mobile Menu on hamburger click:
 
